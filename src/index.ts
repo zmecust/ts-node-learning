7cda0b14e2f4371038ed56c63e4cf6f417eb0786
zmecust <247281377@qq.com>
2019-10-27 11:45:00 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+
+const app: Express = express();
+
+const port: any = process.env.PORT || 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('hello world');
+});
+
+app.listen(port, async () => {
+  await mongoose.connect("mongodb://test:test12345678@ds139768.mlab.com:39768/tsexpress", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  console.log(`Running on http://localhost:${port}`);
+});
